91be47cb2908fa6ba53add0f0797e4ee761670fd
mbeckdev <mbeckdev42@gmail.com>
2022-01-04 17:38:06 -0700

    start initial react-router-dom code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+// import { BrowserRouter as SomeName} from 'react-router-dom';
+
+const User = ({ match }) => {
+  return (
+    <div>
+      <h2>how are you {match.params.username}?</h2>
+    </div>
+  );
+};
+
+const User2 = ({ match }) => {
+  return (
+    <div>
+      <div>asdf</div>
+      <div>lol `${match.params.slug}` </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <main>main here</main>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <div>it's working</div>
+        <Routes>
+          {/* <Route path="/" element={<User />} /> */}
+          {/* <Route path="/user" element={<User ha="user ha" />} /> */}
+          {/* <Route path="/user/:username" element={<User />} /> */}
+          <Route path="/blog/:slug" element={<User2 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+// react router v6 https://www.youtube.com/watch?v=0cSVuySEB0A&t=476s
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Home />
+  </Router>,
   document.getElementById('root')
 );
+
+function Home() {
+  return (
+    <div>
+      <h1>Home router</h1>
+    </div>
+  );
+}
